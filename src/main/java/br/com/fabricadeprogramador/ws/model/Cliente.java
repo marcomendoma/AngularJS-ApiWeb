f91760cc88d6043d0c1402f170f1ef3f34f1aa3a
marcomendoma <marcomendoma@ig.com.br>
2018-01-04 17:27:44 -0200

    alteração entidade cliente

--- src/main/java/br/com/fabricadeprogramador/ws/model/Cliente.java
+++ src/main/java/br/com/fabricadeprogramador/ws/model/Cliente.java
@@ -14,7 +14,7 @@ import javax.persistence.SequenceGenerator;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity(name="cliente")
-public class Cliente { 
+public class Cliente {   
 	
 	@Id
 	@SequenceGenerator(name = "cliente_id", sequenceName = "cliente_seq", initialValue = 1, allocationSize = 1)
